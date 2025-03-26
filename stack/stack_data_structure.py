1cf59b91cd2f46319d45a136b723934fc98a481c
anreddy516 <anreddy516@gmail.com>
2025-03-25 19:04:48 -0500

    Define stack

---
+++ stack/stack_data_structure.py
@@ -0,0 +1,59 @@
+class StackDataStructure: 
+    
+    # Create stack or initialize a stack with some capacity
+    
+    def __init__(self, capacity):
+        self.capacity =  capacity
+        self.top = -1
+        self.stack_storage = [0] * self.capacity
+    
+    # is_stack_full() or stack overflow - when stack is full
+    def is_stack_full(self):
+        if (self.top == self.capacity - 1):
+            return True
+        else:
+            return False
+        
+    # is_stack_empty() or stack underflow - when stack is empty.
+    def is_stack_empty(self):
+        if (self.top == -1):
+            return True
+        else:
+            return False
+    
+    # push() - to add an item at top of a stack
+    def push(self, item):
+        if (self.is_stack_full()):
+            print("Stack is overflow")
+            return False
+        
+        self.top = self.top + 1
+        self.stack_storage[self.top] = item
+        return True
+
+    # Retrieve an item from top
+    def pop(self):
+        if (self.is_stack_empty()):
+            print("Stack is underflow or empty")
+            return -1
+        
+        item = self.stack_storage[self.top]
+        self.top = self.top - 1
+        return item
+    
+    # Get an item from top
+    def peek(self):
+        if (self.is_stack_empty()):
+            print("Stack is underflow or empty")
+            return -1
+        
+        item = self.stack_storage[self.top]
+        return item
+    
+    def print(self):
+        print(f"Capacity = {self.capacity}")
+        print(f"Top      = {self.top}")
+        print("Stack items are here:")
+        for i in range(self.top, -1, -1) :
+            print(self.stack_storage[i])
+        
---
+++ stack/stack_data_structure_test.py
@@ -0,0 +1,41 @@
+from stack_data_structure import StackDataStructure
+
+my_stack = StackDataStructure(4)
+
+choice = int(input("Enter your choice : "))
+
+while(True):
+    if (choice == 6):
+        print("Exited")
+        break
+
+    if (choice == 1):
+        item = int(input("Enter item to be pushed : "))
+        my_stack.push(item)
+        my_stack.print()
+    elif (choice == 2):
+        item = my_stack.pop()
+        print(f"Got item from stack = {item}")
+        my_stack.print()
+    elif (choice == 3):
+        item = my_stack.peek()
+        print(f"Got item from stack = {item}")
+        my_stack.print()
+    elif (choice == 4):
+        overflow = my_stack.is_stack_full()
+        if (overflow):
+            print("Stack is overflow")
+        else:
+            print("Stack is not overflow")
+        my_stack.print()
+    elif (choice == 5):
+        underflow = my_stack.is_stack_empty()
+        if (underflow):
+            print("Stack is underflow")
+        else:
+            print("Stack is not underflow")
+        my_stack.print()
+
+    choice = int(input("Enter your choice : "))
+    
+        
